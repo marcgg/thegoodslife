8db16cf823855bff8062fe255881333907e199ce
Marc G Gauthier <marcg.gauthier@gmail.com>
2012-12-01 22:33:26 +0100

    long lat

--- app/models/step.rb
+++ app/models/step.rb
@@ -3,4 +3,12 @@ class Step < ActiveRecord::Base
   belongs_to :winner, class_name: "User"
   belongs_to :owner, class_name: "User"
   belongs_to :good, inverse_of: :steps
+
+  def long
+    "2.#{self.id}"
+  end
+
+  def lat
+    "48.#{self.id}"
+  end
 end
