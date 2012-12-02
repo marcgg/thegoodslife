82aab221fcfa9b7679924300906681a21d3f180e
Étienne Barrié <etienne.barrie@gmail.com>
2012-12-02 04:53:37 +0100

    Escape location

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,6 +6,6 @@ class SearchController < ApplicationController
   end
 
   def geocoder
-    render text: open("http://maps.googleapis.com/maps/api/geocode/json?address=#{params[:s]}&sensor=false").read
+    render text: open("http://maps.googleapis.com/maps/api/geocode/json?address=#{CGI.escape(params[:s])}&sensor=false").read
   end
 end
