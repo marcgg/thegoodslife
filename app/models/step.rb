4a0c7e6baf618fb46474385d350eb2324e081436
Étienne Barrié <etienne.barrie@gmail.com>
2012-12-02 03:37:59 +0100

    Allow editing lat&long

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,5 +1,5 @@
 class Step < ActiveRecord::Base
-  attr_accessible :good_id, :location, :message, :owner_id, :photo_url, :winner_id, :type, :affected_date
+  attr_accessible :good_id, :location, :message, :owner_id, :photo_url, :winner_id, :type, :affected_date, :lat, :long
   belongs_to :winner, class_name: "User"
   belongs_to :owner, class_name: "User"
   belongs_to :good, inverse_of: :steps
