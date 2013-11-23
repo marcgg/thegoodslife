9454e0b0b1b05e91183ba7efc17dadc378a9a188
Étienne Barrié <etienne.barrie@gmail.com>
2013-11-23 17:00:42 +0100

    Updates

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,10 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Thegoodslife::Application.config.secret_token = 'c37509890c6a96492d34f8dee73c221e4a925793bbe6506fe354b6995fd4136c6bed15ac30662d414e4af084f225fd129ef6d880f491a3220d87c329d060e105'
+Thegoodslife::Application.config.secret_token =
+  case Rails.env
+  when 'production'
+    ENV['SECRET_TOKEN']
+  else
+    'c37509890c6a96492d34f8dee73c221e4a925793bbe6506fe354b6995fd4136c6bed15ac30662d414e4af084f225fd129ef6d880f491a3220d87c329d060e105'
+  end
